5370cfe31a2de76ca5b7e8efbfdb70d072a16c88
Flávio Dantas <fdantasb@gmail.com>
2018-07-23 13:25:17 -0300

    Criados atributos para a classe gasto

--- src/main/java/br/com/fdantasb/domain/Gasto.java
+++ src/main/java/br/com/fdantasb/domain/Gasto.java
@@ -1,4 +1,47 @@
 package br.com.fdantasb.domain;
 
-public class Gasto {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Gasto implements Serializable {
+
+    private static final long serialVersionUID = 7695562818079526722L;
+
+    private Long id;
+    private String descricao;
+    private Double valor;
+    private Date data;
+    //private Usuario user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
 }
