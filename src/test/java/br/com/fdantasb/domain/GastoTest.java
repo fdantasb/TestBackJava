314b7d8dd7847f01533558cc16a66fb1c40fb063
Flávio Dantas <fdantasb@gmail.com>
2018-07-24 12:44:29 -0300

    Criada classe de teste par o gasto

---
+++ src/test/java/br/com/fdantasb/domain/GastoTest.java
@@ -0,0 +1,18 @@
+package br.com.fdantasb.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GastoTest {
+
+    @Test
+    public void dataValida(){
+        
+    }
+}
