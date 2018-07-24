bf5bfd450e10a1c1d2effc81ec51e53fafac63df
Flávio Dantas <fdantasb@gmail.com>
2018-07-24 12:51:41 -0300

    Criado caso de teste para inserção de data inválida

--- src/test/java/br/com/fdantasb/domain/GastoTest.java
+++ src/test/java/br/com/fdantasb/domain/GastoTest.java
@@ -5,14 +5,25 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Calendar;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GastoTest {
 
-    @Test
-    public void dataValida(){
-        
+    @Test(expected = IllegalArgumentException.class)
+    public void dataFutura(){
+
+        Calendar dataFutura = Calendar.getInstance();
+        int hoje = dataFutura.get(Calendar.DAY_OF_MONTH);
+        dataFutura.set(Calendar.DAY_OF_MONTH, hoje + 1);
+
+        Gasto gasto = new Gasto();
+
+        gasto.setData(dataFutura.getTime());
+
+
     }
 }
