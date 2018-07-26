59b11952dfb26059b527afa0c0ef98ee79f46027
Flávio Dantas <fdantasb@gmail.com>
2018-07-26 13:49:39 -0300

    Adicionado o builder da aplicação na classe principal

--- src/main/java/br/com/fdantasb/TestBackJavaApplication.java
+++ src/main/java/br/com/fdantasb/TestBackJavaApplication.java
@@ -2,9 +2,16 @@ package br.com.fdantasb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestBackJavaApplication {
+public class TestBackJavaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(TestBackJavaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestBackJavaApplication.class, args);
